ba41788d1e9ead887f5d4f60ca90359b84b4f4a0
Maria09876 <maria.efthymiou46@gmail.com>
2022-06-06 16:09:10 +0200

    linked weather icon to url/correlating with data

--- src/app.js
+++ src/app.js
@@ -6,25 +6,28 @@ function showTemperature(response) {
     let rangeTemperature = document.querySelector("#tempRange");
     let descriptionElement = document.querySelector("#weather-description");
     let feelsLikeElement = document.querySelector("#feelsLike");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
     city.innerHTML = response.data.name;
     rangeTemperature.innerHTML = `${Math.round(response.data.main.temp_min)}° / ${Math.round(response.data.main.temp_max)}°C`;
     descriptionElement.innerHTML = response.data.weather[0].description;
     feelsLikeElement.innerHTML = `${Math.round(response.data.main.feels_like)}°C`;
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
     //data and variables for the middle section
     let windElement = document.querySelector("#windSpeed");
     let humidityElement = document.querySelector("#humidity");
-    let kilometers = convertWindSpeed(response.data.wind.speed);
+    //let kilometers = convertWindSpeed(response.data.wind.speed);
 
-    windElement.innerHTML = `${kilometers} km/h`;
+    windElement.innerHTML = `${Math.round(response.data.wind.speed * 3.6)} km / h`;
     humidityElement.innerHTML = `${response.data.main.humidity} %`;
 }
 
-function convertWindSpeed(km) {
-  return Math.round(response.data.wind.speed * 3.6);
-  
+//function convertWindSpeed(km) {
+    //return Math.round(response.data.wind.speed * 3.6);
+//}
 
 
 
